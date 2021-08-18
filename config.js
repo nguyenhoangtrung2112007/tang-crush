a28e6b24c82b924b6e157191a402bd309dbf8b7c
nguyenhoangtrung2112007 <86759514+nguyenhoangtrung2112007@users.noreply.github.com>
2021-08-18 11:04:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu từ cái nhìn đầu tiên <33333333',
+    reply: 'Yêu từ cái nhìn đầu tiên ý <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay ib cho anh.',
     btnAccept: 'Okiiiii anh lun <3',

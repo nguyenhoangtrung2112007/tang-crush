54612523eef74746c995a7c461ed5a494e9cba91
nguyenhoangtrung2112007 <86759514+nguyenhoangtrung2112007@users.noreply.github.com>
2021-08-18 11:02:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Em à !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Lạc vào tim anh, lẽ nào đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -11,9 +10,9 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu từ cái nhìn đầu tiên <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay ib cho anh.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii anh lun <3',
     messLink: 'http://m.me/trung.nguyenhoang.37625843' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
